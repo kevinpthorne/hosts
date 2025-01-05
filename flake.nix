14f8080da76f31c8e2da95c8b62ff5b6a0587697
Kevin Thorne <me@kevinpthorne.com>
2025-01-04 23:51:48 -0500

    fix: gnome fix from https://github.com/LucasFA/.nixos/commit/9cfda103f9781ad02af08b98efe04a61f3d7e7bc

--- flake.nix
+++ flake.nix
@@ -13,15 +13,17 @@
       # to avoid problems caused by different versions of nixpkgs.
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    gnome-fix.url = "github:LucasFA/.nixos/9cfda103f9781ad02af08b98efe04a61f3d7e7bc";
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, gnome-fix, ... }@inputs: {
     nixosConfigurations.desktop1 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         # Import the previous configuration.nix we used,
         # so the old configuration file still takes effect
         ./hosts/desktop1/configuration.nix
+        <gnome-fix/hosts/slimbook-hero/gnome-fix.nix>
 	# make home-manager as a module of nixos
         # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
         home-manager.nixosModules.home-manager
